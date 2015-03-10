c4e45e6f0913c53114961f77273e51be641658de
unknown <abelevich@gmail.com>
2015-03-10 07:51:20 -0700

    Rename project

--- gen/com/abv/deletebookmarks/R.java
+++ gen/com/abv/deletebookmarks/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.abv.bookmarkcleaner;
+package com.abv.deletebookmarks;
 
 public final class R {
     public static final class attr {
--- src/com/abv/deletebookmarks/MainActivity.java
+++ src/com/abv/deletebookmarks/MainActivity.java
@@ -1,4 +1,4 @@
-package com.abv.bookmarkcleaner;
+package com.abv.deletebookmarks;
 
 import android.net.Uri;
 import android.os.Bundle;
